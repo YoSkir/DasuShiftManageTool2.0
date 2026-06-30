eca3fb3664649f7bced7661ef5107d28eba420c1
YoSkir <137788381+YoSkir@users.noreply.github.com>
2026-06-30 16:32:39 +0800

    feat(): 建立資料庫抽象方法

--- DasuShiftManager/Code/Entities/Setting.cs
+++ DasuShiftManager/Code/Entities/Setting.cs
@@ -2,15 +2,15 @@
 
 public class Setting
 {
-    public int ShiftStartDay { get; set; }
-    public TimeOnly ShiftStartTime { get; set; }
-    public int ShiftHalfHourCount { get; set; }
-    public int FirstBreakActiveWorkHalfHrs { get; set; }
-    public int FirstBreakDurationHalfHrs {get; set;}
-    public int SecondBreakActiveWorkHalfHrs { get; set; }
-    public int SecondBreakDurationHalfHrs { get; set; }
-    public List<int> ShiftHalfHourType { get; set; } = [12,16,20,24];
-    public Dictionary<int, int> EveryHalfHourMinWorkers { get; set; } = new();
-    public Dictionary<int, int> EveryHalfHourMinManagersOrPharmacist { get; set; } = new();
-    public int MaxChainWorkDays { get; set; }
+    public int ShiftStartDay { get; init; }
+    public TimeOnly ShiftStartTime { get; init; }
+    public int ShiftHalfHourCount { get; init; }
+    public int FirstBreakActiveWorkHalfHrs { get; init; }
+    public int FirstBreakDurationHalfHrs {get; init;}
+    public int SecondBreakActiveWorkHalfHrs { get; init; }
+    public int SecondBreakDurationHalfHrs { get; init; }
+    public List<int> ShiftHalfHourType { get; init; } = [12,16,20,24];
+    public Dictionary<int, int> EveryHalfHourMinWorkers { get; init; } = new();
+    public Dictionary<int, int> EveryHalfHourMinManagersOrPharmacist { get; init; } = new();
+    public int MaxChainWorkDays { get; init; }
 }
